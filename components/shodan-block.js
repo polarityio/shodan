f60661c54642c2a0f29b71a14baeb1d51d7e16fe
Ed <ed@polarity.io>
2020-04-07 23:23:41 -0400

    Update template to focus on most useful information

--- components/shodan-block.js
+++ components/shodan-block.js
@@ -1,4 +1,9 @@
 polarity.export = PolarityComponent.extend({
     details: Ember.computed.alias('block.data.details'),
-
+    showDetails: false,
+    actions: {
+        toggleDetails: function(){
+            this.toggleProperty('showDetails');
+        }
+    }
 });
--- components/shodan-summary.js
+++ components/shodan-summary.js
@@ -1,35 +1,42 @@
 'use strict';
 
 polarity.export = PolarityComponent.extend({
-    details: Ember.computed.alias('block.data.details'),
-    summaryTags: Ember.computed('details.tags', function(){
-        let summaryTags = [];
+  details: Ember.computed.alias('block.data.details'),
+  summaryTags: Ember.computed('details.tags', function() {
+    let summaryTags = [];
 
-        if(this.get('details.city')){
-            summaryTags.push(this.get('details.city'));
-        }
+    let location = '';
+    const country = this.get('details.country_name');
+    const city = this.get('details.city');
 
-        if(this.get('details.isp')){
-            summaryTags.push(this.get('details.isp'));
-        }
+    if (city && country) {
+      location = `${city}, ${country}`;
+    } else if (city) {
+      location = city;
+    } else if (country) {
+      location = country;
+    }
 
-        if(this.get('details.country_name')){
-            summaryTags.push(this.get('details.country_name'));
-        }
+    if (location) {
+      summaryTags.push(location);
+    }
 
-        if(this.get('details.org')){
-            summaryTags.push(this.get('details.org'));
-        }
+    if (this.get('details.isp')) {
+      summaryTags.push(`ISP: ${this.get('details.isp')}`);
+    }
 
-        let tags = this.get('details.tags');
-        if(Array.isArray(tags)){
-            summaryTags = summaryTags.concat(tags);
-        }
+    if (this.get('details.org')) {
+      summaryTags.push(`Org: ${this.get('details.org')}`);
+    }
 
-        if(summaryTags.length === 0){
-            summaryTags.push('No Tags');
-        }
-        return summaryTags;
-    })
+    let tags = this.get('details.tags');
+    if (Array.isArray(tags)) {
+      summaryTags = summaryTags.concat(tags);
+    }
+
+    if (summaryTags.length === 0) {
+      summaryTags.push('No Tags');
+    }
+    return summaryTags;
+  })
 });
-
