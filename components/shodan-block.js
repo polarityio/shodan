3dfe06c88f122e5bc2d0aeae13dd09cc828dae58
ntsmith7 <nick@polarity.io>
2023-02-22 12:29:36 -0800

    feat: add copy data button

--- components/shodan-block.js
+++ components/shodan-block.js
@@ -1,14 +1,22 @@
 polarity.export = PolarityComponent.extend({
   data: Ember.computed.alias('block.data'),
   details: Ember.computed.alias('block.data.details'),
-  timezone: Ember.computed("Intl", function () {
+  timezone: Ember.computed('Intl', function () {
     return Intl.DateTimeFormat().resolvedOptions().timeZone;
   }),
   showDetails: false,
+  showCopyMessage: false,
+  uniqueIdPrefix: '',
   entity: Ember.computed.alias('block.entity'),
   message: '',
   errorMessage: null,
   isRunning: false,
+  init () {
+    let array = new Uint32Array(5);
+    this.set('uniqueIdPrefix', window.crypto.getRandomValues(array).join(''));
+
+    this._super(...arguments);
+  },
   actions: {
     toggleDetails: function () {
       this.toggleProperty('showDetails');
@@ -35,6 +43,45 @@ polarity.export = PolarityComponent.extend({
           this.set('isRunning', false);
           outerThis.get('block').notifyPropertyChange('data');
         });
+    },
+    copyData: function () {
+      const savedDetails = this.get('block._state.showDetails');
+
+      console.log(this.get('details'));
+
+      Ember.run.scheduleOnce(
+        'afterRender',
+        this,
+        this.copyElementToClipboard,
+        `shodan-container-${this.get('uniqueIdPrefix')}`
+      );
+
+      Ember.run.scheduleOnce('destroy', this, this.restoreCopyState, savedDetails);
     }
+  },
+  copyElementToClipboard (element) {
+    window.getSelection().removeAllRanges();
+    let range = document.createRange();
+
+    range.selectNode(typeof element === 'string' ? document.getElementById(element) : element);
+    window.getSelection().addRange(range);
+    document.execCommand('copy');
+    window.getSelection().removeAllRanges();
+  },
+  getElementRance (element) {
+    let range = document.createRange();
+    range.selectNode(typeof element === 'string' ? document.getElementById(element) : element);
+    return range;
+  },
+  restoreCopyState (savedDetails) {
+    this.set('showCopyMessage', true);
+
+    this.set('block._state.showDetails', savedDetails);
+
+    setTimeout(() => {
+      if (!this.isDestroyed) {
+        this.set('showCopyMessage', false);
+      }
+    }, 2000);
   }
 });
